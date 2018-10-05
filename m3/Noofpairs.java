a22914c487d17a6fc3e49c7ec901dc3f67078d3c
Manogna40 <43471477+Manogna40@users.noreply.github.com>
2018-10-05 18:03:17 +0530

    Initial commit

---
+++ m3/Noofpairs.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Noofpairs {
+
+	public static void main(String[] args) {
+		int[] a = null;
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter no of elements:");
+		int n=s.nextInt();
+		a=new int[n];
+		for(int i = 0;i<n;i++)
+		{
+			System.out.println("enter "+(i+1)+" element");
+			a[i]=s.nextInt();
+		}
+	Arrays.sort(a);
+	int count=0;
+	for(int i = 0;i<n-1;i++)
+	{
+		if(a[i]==a[i+1])
+		{
+			count++;
+		}
+	}
+	System.out.println(" no of pairs of numvers is "+count);
+	}
+}
+
