9991a017b37d28ebe29e1d7157800932362deba8
Manogna40 <43471477+Manogna40@users.noreply.github.com>
2018-10-07 21:52:19 +0530

    new

--- m3/Noofpairs.java
+++ m3/Noofpairs.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Noofpairs {
-
-	public static void main(String[] args) {
-		int[] a = null;
-		Scanner s= new Scanner(System.in);
-		System.out.println("enter no of elements:");
-		int n=s.nextInt();
-		a=new int[n];
-		for(int i = 0;i<n;i++)
-		{
-			System.out.println("enter "+(i+1)+" element");
-			a[i]=s.nextInt();
-		}
-	Arrays.sort(a);
-	int count=0;
-	for(int i = 0;i<n-1;i++)
-	{
-		if(a[i]==a[i+1])
-		{
-			count++;
-		}
-	}
-	System.out.println(" no of pairs of numvers is "+count);
-	}
-}
-
-=======
-package m3;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -58,5 +25,3 @@ public class Noofpairs {
 	System.out.println(" no of pairs of numvers is "+count);
 	}
 }
-
->>>>>>> f0781cc8fff40f6bcd6da6596c552137dd5954ff
--- m3/Sum3.java
+++ m3/Sum3.java
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-package m3;
-
-import java.util.Scanner;
-
-public class Sum3 {
-
-	public static void main(String[] args) {Scanner sc = new Scanner(System.in);
-	int count=0;
-	int i=0,j,k,l;
-	System.out.println("enter size");
-	int n=sc.nextInt();
-int  q[]=new int[n];
-
-		while(sc.hasNext() ) {
-			 q[i] =  sc.nextInt();
-			 i++;
-		}
-		for(j=0;j<q.length;j++)
-		{
-			for(k=(j+1);k<q.length;k++)
-			{
-				for(l=(k+1);l<q.length;l++)
-				{
-					if(q[j]+q[k]+q[l]==0)
-					{
-						count++;
-						break;
-					}
-				}
-			}
-		}
-		System.out.println(count);
-	}
-	
-
-}
-=======
-package m3;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -74,4 +35,3 @@ int  q[]=new int[n];
 	
 
 }
->>>>>>> f0781cc8fff40f6bcd6da6596c552137dd5954ff
